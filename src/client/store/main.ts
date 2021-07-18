b17211ac9398b9ac55c68c5ccf9906fc637032b8
Coderbora <coderbora@gmail.com>
2021-07-18 11:06:57 +0300

    initial showsettings store

--- src/client/store/main.ts
+++ src/client/store/main.ts
@@ -2,13 +2,15 @@ import { createStore } from 'vuex';
 import createPersistedState from "vuex-persistedstate";
 import * as types from './main_types';
 
-import { Map } from '../../shared/types';
+import { Map, ShowSettings } from '../../shared/types';
 
 export interface MainState {
     initialized: boolean,
     apiKey: string,
 
     showcasePool: Map[],
+
+    showSettings: ShowSettings,
 }
 
 export const store = createStore<MainState>({
@@ -18,7 +20,157 @@ export const store = createStore<MainState>({
     state: {
         initialized: false,
         apiKey: "",
-        showcasePool: []
+        showcasePool: [],
+
+        showSettings: {
+            normalColor: "",
+            accentColor: "",
+            textFont: "",
+
+            map: {
+                listImage: {
+                    enabled: true,
+                    top: 0,
+                    left: 0,
+                    mode: "normal"
+                },
+
+                title: {
+                    enabled: true,
+                    top: 0,
+                    left: 0,
+                    mode: "normal"
+                },
+                songName: {
+                    enabled: true,
+                    top: 0,
+                    left: 0,
+                    mode: "normal"
+                },
+                version: {
+                    enabled: true,
+                    top: 0,
+                    left: 0,
+                    mode: "normal"
+                },
+                
+                cs: {
+                    enabled: true,
+                    top: 0,
+                    left: 0,
+                    mode: "normal"
+                },
+                ar: {
+                    enabled: true,
+                    top: 0,
+                    left: 0,
+                    mode: "normal"
+                },
+                od: {
+                    enabled: true,
+                    top: 0,
+                    left: 0,
+                    mode: "normal"
+                },
+                hp: {
+                    enabled: true,
+                    top: 0,
+                    left: 0,
+                    mode: "normal"
+                },
+                length: {
+                    enabled: true,
+                    top: 0,
+                    left: 0,
+                    mode: "normal"
+                },
+                bpm: {
+                    enabled: true,
+                    top: 0,
+                    left: 0,
+                    mode: "normal"
+                },
+                sr: {
+                    enabled: true,
+                    top: 0,
+                    left: 0,
+                    mode: "normal"
+                },
+            
+                mapID: {
+                    enabled: true,
+                    top: 0,
+                    left: 0,
+                    mode: "normal"
+                },
+            },
+
+            player: {
+                playerAvatar: {
+                    enabled: true,
+                    top: 0,
+                    left: 0,
+                    mode: "normal"
+                },
+    
+                username: {
+                    enabled: true,
+                    top: 0,
+                    left: 0,
+                    mode: "normal"
+                },
+                rank: {
+                    enabled: true,
+                    top: 0,
+                    left: 0,
+                    mode: "normal"
+                },
+                
+                count300: {
+                    enabled: true,
+                    top: 0,
+                    left: 0,
+                    mode: "normal"
+                },
+                count100: {
+                    enabled: true,
+                    top: 0,
+                    left: 0,
+                    mode: "normal"
+                },
+                count50: {
+                    enabled: true,
+                    top: 0,
+                    left: 0,
+                    mode: "normal"
+                },
+                countMiss: {
+                    enabled: true,
+                    top: 0,
+                    left: 0,
+                    mode: "normal"
+                },
+            
+                maxCombo: {
+                    enabled: true,
+                    top: 0,
+                    left: 0,
+                    mode: "normal"
+                },
+                accuracy: {
+                    enabled: true,
+                    top: 0,
+                    left: 0,
+                    mode: "normal"
+                },
+                score: {
+                    enabled: true,
+                    top: 0,
+                    left: 0,
+                    mode: "normal"
+                },
+            }
+        }
     },
     mutations: {
         [types.SET_INITIALIZED] (state) {
--- src/shared/types.ts
+++ src/shared/types.ts
@@ -2,6 +2,7 @@ import { OsuMod } from "@brunohpaiva/osu-parser";
 
 export type Grade = "XH" | "X" | "SH" | "S" | "A" | "B" | "C" | "D";
 export type Modpool = "NM" | "HD" | "HR" | "DT" | "FM" | "TB";
+export type ShowType = "normal" | "accent";
 export interface Map extends Record<string, unknown> {
     id: number,
 
@@ -42,4 +43,55 @@ export interface Player {
     accuracy: number,
     score: number,
     modsUsed: OsuMod[],
+}
+
+export interface ComponentDetails {
+    enabled: boolean,
+    top: number,
+    left: number,
+    mode: ShowType
+}
+
+export interface ShowMapDetails {
+    listImage: ComponentDetails,
+
+    title: ComponentDetails,
+    songName: ComponentDetails,
+    version: ComponentDetails,
+    
+    cs: ComponentDetails,
+    ar: ComponentDetails,
+    od: ComponentDetails,
+    hp: ComponentDetails,
+    length: ComponentDetails,
+    bpm: ComponentDetails,
+    sr: ComponentDetails,
+
+    mapID: ComponentDetails,
+}
+
+export interface ShowPlayerDetails {
+    playerAvatar: ComponentDetails,
+
+    username: ComponentDetails,
+    rank: ComponentDetails,
+    
+    count300: ComponentDetails,
+    count100: ComponentDetails,
+    count50: ComponentDetails,
+    countMiss: ComponentDetails,
+
+    maxCombo: ComponentDetails,
+    accuracy: ComponentDetails,
+    score: ComponentDetails,
+}
+
+export interface ShowSettings {
+    normalColor: string,
+    accentColor: string,
+    textFont: string,
+
+    map: ShowMapDetails,
+
+    player: ShowPlayerDetails,
 }
